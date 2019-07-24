bac28df994b57e094577ce63a0a335a629d41946
Matthew Brett <matthew.brett@gmail.com>
2019-07-24 10:44:56 +0100

    Update make_circles

--- make_circles.py
+++ make_circles.py
@@ -1,6 +1,6 @@
-import math
 from itertools import cycle
-from random import shuffle
+from collections import Counter
+from random import shuffle, seed
 
 import numpy as np
 
@@ -8,47 +8,49 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
 
-class Logo:
-
-    def __init__(self, start_x, start_y, **kwargs):
-        self.x = start_x
-        self.y = start_y
-        self.kwargs = kwargs.copy()
-
-    def move_to(self, x, y):
-        plt.plot([self.x, x], [self.y, y],
-                  **self.kwargs)
-        self.x = x
-        self.y = y
-
-    def move_relative(self, rel_x, rel_y):
-        self.move_to(self.x + rel_x, self.y + rel_y)
-
-
 class BallShower:
 
-    def __init__(self, n_balls1, n_balls2,
-                ball_txt_fontsize=16,
-                ball_txt_color='white'):
-        self.n_balls1 = n_balls1
-        self.n_balls2 = n_balls2
+    def __init__(self, rows, cols,
+                 ball_txt_fontsize=16,
+                 ball_txt_color='white'):
+        self.rows = rows
+        self.cols = cols
         self.ball_txt_fontsize=ball_txt_fontsize
         self.ball_txt_color=ball_txt_color
-        self.n_balls = n_balls1 + n_balls2
-        self.rows = self.cols = math.ceil(math.sqrt(self.n_balls))
         diameter = 1 / self.rows
         self.radius = diameter / 2
         self.centers_x = np.arange(self.rows) * diameter + self.radius
         self.centers_y = (np.arange(self.cols) * diameter + self.radius)[::-1]
+        self._ax = None
+        self._balls = []
 
     def ball_center(self, ball_no):
         ball_row = ball_no // self.cols
         ball_col = ball_no % self.cols
         return self.centers_x[ball_col], self.centers_y[ball_row]
 
-    def ball_figure(self, balls):
-        fig = plt.figure()
-        ax = plt.gca()
+    def ball_edges(self, ball_no):
+        c_x, c_y = self.ball_center(ball_no)
+        r = self.radius
+        return (((c_x - r,  c_x - r), (c_y - r, c_y + r)),
+                ((c_x - r,  c_x + r), (c_y + r, c_y + r)),
+                ((c_x - r,  c_x + r), (c_y - r, c_y - r)),
+                ((c_x + r,  c_x + r), (c_y - r, c_y + r)),
+               )
+
+    def _round_edge(self, edge, precision):
+        return tuple(tuple(np.round(cs, precision)) for cs in edge)
+
+    def bounding_edges(self, ball_nos, precision=6):
+        all_edges = sum([self.ball_edges(b) for b in ball_nos], ())
+        all_edges = [self._round_edge(es, precision) for es in all_edges]
+        edge_counts = Counter(all_edges)
+        return tuple(e for e, c in edge_counts.items() if c == 1)
+
+    def ball_figure(self, balls, ax=None):
+        if ax is None:
+            plt.figure()
+            ax = plt.gca()
         for ball_no, ball in enumerate(balls):
             c_x, c_y = self.ball_center(ball_no)
             bugs, color = balls[ball_no]
@@ -61,55 +63,30 @@ class BallShower:
                     color=self.ball_txt_color)
             ax.add_patch(p)
         ax.axis('off')
-        return ax
+        self._balls = balls
+        self._ax = ax
 
+    def plot_bb(self, ball_nos, color='red', linewidth=4):
+        for xs, ys in self.bounding_edges(ball_nos):
+            self._ax.plot(xs, ys, color=color, linewidth=linewidth)
 
-def show_balls(balls1, balls2, border_color='red'):
-    plt.figure()
-    ax = plt.gca()
-    balls = balls1 + balls2
-    n_balls = len(balls)
-    # Put into square box
-    rows = cols = math.ceil(math.sqrt(n_balls))
-    diameter = 1 / rows
-    radius = diameter / 2
-    centers_x = np.arange(rows) * diameter + radius
-    centers_y = (np.arange(cols) * diameter + radius)[::-1]
-    b_no = 0
-    for c_y in centers_y:
-        for c_x in centers_x:
-            bugs, color = balls[b_no]
-            p = mpatches.Circle((c_x, c_y), radius, color=color)
-            plt.text(c_x, c_y,
-                     bugs,
-                     horizontalalignment='center',
-                     verticalalignment='center',
-                     fontsize=16,
-                     color='white')
-            ax.add_patch(p)
-            b_no += 1
-            if b_no == n_balls:
-                break
-    # Draw box around first set of balls
-    last_balls1 = len(balls1) - 1
-    row_last_first = last_balls1 // cols
-    col_last_first = last_balls1 % cols
-    row_pos = 1 - (row_last_first + 1) * diameter
-    col_pos = (col_last_first + 1) * diameter
-    logo = Logo(0, 1, color=border_color, linewidth=4)
-    logo.move_to(0, row_pos)
-    logo.move_to(col_pos, row_pos)
-    if col_last_first != cols:
-        logo.move_relative(0, diameter)
-        logo.move_relative(1 - col_pos, 0)
-    logo.move_to(1, 1)
-    logo.move_to(0, 1)
-    ax.axis('off')
+    def mean_for(self, ball_nos, plot_ball, color='black'):
+        values = [self._balls[i][0] for i in ball_nos]
+        mu = np.mean(values)
+        c_x, c_y = self.ball_center(plot_ball)
+        self._ax.text(c_x, c_y,
+                      'mean = {:.2f}'.format(mu),
+                      horizontalalignment='center',
+                      verticalalignment='center',
+                      fontsize=self.ball_txt_fontsize,
+                      color=color)
+        return mu
 
 
 beer_attract = [27, 20, 21, 26, 27, 31, 24, 21, 20,
                 19, 23, 24, 28, 19, 24, 29, 18,
                 20, 17, 31, 20, 25, 28, 21, 27]
+n_beer = len(beer_attract)
 beer_balls = list(zip(beer_attract, cycle(['#cc9900'])))
 
 water_attract = [21, 22, 15, 12, 21, 16,
@@ -117,7 +94,39 @@ water_attract = [21, 22, 15, 12, 21, 16,
                  13, 22, 20, 24, 18, 20]
 water_balls = list(zip(water_attract, cycle(['#33bbff'])))
 balls = beer_balls + water_balls
+n_balls = len(balls)
 
-bshower = BallShower(len(beer_balls), len(water_balls))
+bshower = BallShower(7, 7)
+beer_nos = range(n_beer)
+water_nos = range(n_beer, n_balls)
+empty_ball = n_balls + 2
+
 bshower.ball_figure(balls)
-plt.show()
+plt.savefig('just_balls.png')
+
+bshower.plot_bb(beer_nos)
+beer_m = bshower.mean_for(beer_nos, empty_ball)
+plt.savefig('beer_mean.png')
+
+bshower.ball_figure(balls)
+bshower.plot_bb(water_nos)
+water_m = bshower.mean_for(water_nos, empty_ball)
+plt.savefig('water_mean.png')
+print(f'Actual difference: {beer_m - water_m:0.2f}')
+
+seed(42)
+shuffled = balls[:]
+
+for i in range(3):
+    shuffle(shuffled)
+    bshower.ball_figure(shuffled)
+    bshower.plot_bb(beer_nos)
+    fb_m = bshower.mean_for(beer_nos, empty_ball)
+    plt.savefig(f'fake_beer_mean{i}.png')
+
+    bshower.ball_figure(shuffled)
+    bshower.plot_bb(water_nos)
+    fw_m = bshower.mean_for(water_nos, empty_ball)
+    plt.savefig(f'fake_water_mean{i}.png')
+
+    print(f'Fake difference {i}: {fb_m - fw_m:0.2f}')
